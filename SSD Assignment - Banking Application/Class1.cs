4b51286f812b868d02bdbe7fb23d308f9ed4300a
KianReynolds <S00237055@atu.ie>
2025-12-19 03:11:36 +0000

    Who and What created

--- SSD Assignment - Banking Application/Class1.cs
+++ SSD Assignment - Banking Application/Class1.cs
@@ -11,6 +11,39 @@ namespace SSD_Assignment___Banking_Application
         private  const string SourceName = "SSD Banking Application";
         private const string LogName = "Application";
 
+        static Logger()
+        {
+            try
+            {
+                if (!EventLog.SourceExists(SourceName))
+                {
+                    EventLog.CreateSource(SourceName, LogName);
 
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Logger Error: Could not create event source. {ex.Message}");
+                Console.WriteLine("Try running Visual Studio as Admin.");
+            }
+        }
+
+        public static void LogTransaction(string tellerName, string accountNo, string accountHolder, string transactionType, string amount = "N/A", string = "N/A")
+        {
+            StringBuilder sb = new StringBuilder();
+
+            // WHO
+            sb.AppendLine($"Teller: {tellerName}");
+            sb.AppendLine($"Account No: {accountNo}");
+            sb.AppendLine($"Account Holder: {accountHolder}");
+
+            // WHAT
+            sb.AppendLine($"Transaction Type: {transactionType}");
+            if (amount != "N/A")
+                sb.AppendLine($"Amount: {amount}");
+
+
+        }
     }
 }
