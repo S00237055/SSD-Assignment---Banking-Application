9eef2c600593264b14a886b89157aff95e7ec065
KianReynolds <S00237055@atu.ie>
2025-12-19 03:03:58 +0000

    Set up Logger Class

---
+++ SSD Assignment - Banking Application/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSD_Assignment___Banking_Application
+{
+    public static class Logger
+    {
+        private  const string SourceName = "SSD Banking Application";
+        private const string LogName = "Application";
+
+
+    }
+}
